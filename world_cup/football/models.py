610439d32710ceeca7d87ac21675e689cb944cd9
alirahmani93 <ali93rahmani@gmail.com>
2022-10-28 20:22:24 +0330

    football calculate result and Player predictions

--- world_cup/football/models.py
+++ world_cup/football/models.py
@@ -1,8 +1,10 @@
+from django.contrib.postgres.fields import ArrayField
 from django.utils.translation import gettext_lazy as _
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 from common.models import BaseModel
+from common.utils.time import get_now
 from football.choices import Continent, WorldCupGroup, MatchLevel, MatchStatus, WinnerChoices, TeamPlayerRole
 
 
@@ -54,7 +56,7 @@ class Match(BaseModel):
     is_penalty = models.BooleanField(verbose_name=_('is penalty'), default=False)
 
     def __str__(self):
-        return f'{self.team_1}-{self.team_2} ({self.level})'
+        return f'{self.team_1}-{self.team_2} (level:{self.level})'
 
     def save(self, *args, **kwargs):
         if self.status == MatchStatus.NOT_STARTED:
@@ -63,34 +65,116 @@ class Match(BaseModel):
         if self.level != MatchLevel.PRELIMINARY:
             if self.winner == WinnerChoices.DRAW:
                 raise Exception("Draw in this level impossible!")
-        super(Match, self).save(args, kwargs)
-
         if self.status == MatchStatus.FINISHED:
+            print(self.teamplayeraction_set.filter(is_best_player=True).count())
+            if self.teamplayeraction_set.filter(is_best_player=True).count() != 1:
+                raise ValueError('One Player must be a best player')
+
+            if not self.winner:
+                raise Exception("Who is winner?!")
             self.match_finish()
+        super(Match, self).save(args, kwargs)
 
     class Meta:
         verbose_name = _("Match")
         verbose_name_plural = _("Matches")
 
     def match_finish(self):
-        r1, r2 = self.calculate_match_result()
-        MatchResult.objects.create(match=self, winner=self.winner, team_1=r1, team_2=r2, is_penalty=self.is_penalty)
-        print('match result created')
+        t1 = get_now()
 
-    def calculate_match_result(self):
-        team_players_1 = TeamPlayerAction.objects.filter(player__team=self.team_1)
-        team_players_2 = TeamPlayerAction.objects.filter(player__team=self.team_2)
+        team_players_1 = self.teamplayeraction_set.filter(player__team=self.team_1)
+        team_players_2 = self.teamplayeraction_set.filter(player__team=self.team_2)
 
-        result_team_1 = self.calculate_match_player_result(team_players_1)
-        result_team_2 = self.calculate_match_player_result(team_players_2)
-        return result_team_1, result_team_2
+        tg_1, bpi_1, gl_1, ga_1, ar_1, ch_1, yc_1, rc_1 = self.calculate_match_player_result(team_players_1)
+        tg_2, bpi_2, gl_2, ga_2, ar_2, ch_2, yc_2, rc_2 = self.calculate_match_player_result(team_players_2)
+        bpi = bpi_1 if bpi_1 else bpi_2
+
+        x = MatchResult.objects.filter(match=self, )
+        if x.exists():
+            x.update(
+                winner=self.winner,
+                is_penalty=self.is_penalty,
+                best_player_id=bpi,
+                team_1_goals=tg_1,
+                team_2_goals=tg_2,
+                arrange_1_list=ar_1,
+                arrange_2_list=ar_2,
+                goal_1_list=gl_1,
+                goal_2_list=gl_2,
+                goal_assist_1_list=ga_1,
+                goal_assist_2_list=ga_2,
+                yellow_card_1_list=yc_1,
+                yellow_card_2_list=yc_2,
+                red_card_1_list=rc_1,
+                red_card_2_list=rc_2,
+                change_1_list=ch_1,
+                change_2_list=ch_2,
+            )
+        else:
+            MatchResult.objects.create(
+                match=self,
+                winner=self.winner,
+                is_penalty=self.is_penalty,
+                best_player_id=bpi,
+                team_1_goals=tg_1,
+                team_2_goals=tg_2,
+                arrange_1_list=ar_1,
+                arrange_2_list=ar_2,
+                goal_1_list=gl_1,
+                goal_2_list=gl_2,
+                goal_assist_1_list=ga_1,
+                goal_assist_2_list=ga_2,
+                yellow_card_1_list=yc_1,
+                yellow_card_2_list=yc_2,
+                red_card_1_list=rc_1,
+                red_card_2_list=rc_2,
+                change_1_list=ch_1,
+                change_2_list=ch_2,
+            )
+
+        print(f'{self.__str__()}: MatchResult created at:{get_now()}')
+        print(f'Process time :{get_now() - t1}') \
 
     @staticmethod
-    def calculate_match_player_result(team_players):
-        tmp = []
-        for tp in team_players:
-            tmp.append(tp.make_dict())
-        return tmp
+    def calculate_match_player_result(team_players_action):
+        best_player_id = 0
+        total_goal = 0
+        goals = []
+        assist_goals = []
+        arrange = []
+        changes = []
+        yellow_cards = []
+        red_cards = []
+        for tp in team_players_action:
+            print('>>>>>>>', tp.goal, tp.assist_goal, tp.yellow_card)
+            arrange.append(tp.id)
+            total_goal += tp.goal
+
+            if tp.goal:
+                goals.append(tp.id)
+            if tp.assist_goal:
+                assist_goals.append(tp.id)
+            if tp.yellow_card:
+                yellow_cards.append(tp.id)
+            if tp.red_card:
+                red_cards.append(tp.id)
+            if tp.is_change:
+                changes.append(tp.id)
+
+            if not best_player_id:
+                if tp.is_best_player:
+                    best_player_id = tp.player.id
+
+        return (
+            total_goal,
+            best_player_id,
+            sorted(goals),
+            sorted(assist_goals),
+            sorted(arrange),
+            sorted(changes),
+            sorted(yellow_cards),
+            sorted(red_cards)
+        )
 
 
 class TeamPlayerAction(BaseModel):
@@ -98,8 +182,7 @@ class TeamPlayerAction(BaseModel):
     player = models.ForeignKey(verbose_name=_('player'), to=TeamPlayer, on_delete=models.CASCADE)
     role = models.IntegerField(verbose_name=_("role"), choices=TeamPlayerRole.choices)
 
-    yellow_card = models.PositiveSmallIntegerField(verbose_name=_("yellow card"), default=0,
-                                                   validators=[MaxValueValidator(2)])
+    yellow_card = models.BooleanField(verbose_name=_("yellow card"), default=False)
     red_card = models.BooleanField(verbose_name=_("red card"), default=False, )
 
     goal = models.PositiveSmallIntegerField(verbose_name=_("goal"), default=0, )
@@ -107,6 +190,13 @@ class TeamPlayerAction(BaseModel):
 
     is_change = models.BooleanField(verbose_name=_("is change"), default=False, )
     is_best_player = models.BooleanField(verbose_name=_("is best player"), default=False)
+
+    @property
+    def team_name(self) -> str:
+        return self.player.team.name
+
+    def save(self, *args, **kwargs):
+        super(TeamPlayerAction, self).save(args, kwargs)
 
     class Meta:
         verbose_name = _("Action Team Player")
@@ -124,7 +214,6 @@ class TeamPlayerAction(BaseModel):
             'yellow_card': self.yellow_card,
             'goal': self.goal,
             'assist_goal': self.assist_goal,
-
             'red_card': self.red_card,
             'is_change': self.is_change,
             'is_best_player': self.is_best_player,
@@ -135,8 +224,27 @@ class MatchResult(BaseModel):
     match = models.OneToOneField(verbose_name=_("match"), to=Match, on_delete=models.CASCADE)
     winner = models.IntegerField(verbose_name=_("winner"), choices=WinnerChoices.choices)
     is_penalty = models.BooleanField(verbose_name=_('is penalty'), default=False)
-    team_1 = models.JSONField(verbose_name=_("team 1"), )
-    team_2 = models.JSONField(verbose_name=_("team 2"), )
+    team_1_goals = models.PositiveIntegerField(verbose_name=_("team 1 goals"), )
+    team_2_goals = models.PositiveIntegerField(verbose_name=_("team 2 goals"), )
+    best_player_id = models.IntegerField(verbose_name=_("best player id"))
+
+    arrange_1_list = ArrayField(verbose_name=_("arrange 1 list"), base_field=models.IntegerField())
+    arrange_2_list = ArrayField(verbose_name=_("arrange 2 list"), base_field=models.IntegerField())
+    goal_1_list = ArrayField(verbose_name=_("goal 1 list"), base_field=models.IntegerField())
+    goal_2_list = ArrayField(verbose_name=_("goal 2 list"), base_field=models.IntegerField())
+    goal_assist_1_list = ArrayField(verbose_name=_("goal assist 1 list"), base_field=models.IntegerField())
+    goal_assist_2_list = ArrayField(verbose_name=_("goal assist 2 list"), base_field=models.IntegerField())
+    yellow_card_1_list = ArrayField(verbose_name=_("yellow card 1 list"), base_field=models.IntegerField())
+    yellow_card_2_list = ArrayField(verbose_name=_("yellow card 2 list"), base_field=models.IntegerField())
+    red_card_1_list = ArrayField(verbose_name=_("red card 1 list"), base_field=models.IntegerField())
+    red_card_2_list = ArrayField(verbose_name=_("red card 2 list"), base_field=models.IntegerField())
+    change_1_list = ArrayField(verbose_name=_("change 1 list"), base_field=models.IntegerField())
+    change_2_list = ArrayField(verbose_name=_("change 2 list"), base_field=models.IntegerField())
+
+    is_processed = models.BooleanField(verbose_name=_('is processed'), default=False,
+                                       help_text='after all player predicts calculated change to True')
+
+    # def save(self,*args,**kwargs):
 
     class Meta:
         verbose_name = _("Match Result")
--- world_cup/football/signals.py
+++ world_cup/football/signals.py
@@ -1,36 +1,85 @@
-from football.choices import MatchStatus
-from football.models import Match, TeamPlayerAction
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.db.models import Sum, Min
-from django.core.cache import cache
-from django.conf import settings
 
-from football.prediction import calculate_points
-from user.models import Player, PredictionArrange
+from common.models import CorrectPredictScore
+from football.choices import MatchStatus
+from football.models import MatchResult, Match
+
+from user.models import PredictionArrange
 
 
 @receiver(post_save, sender=Match)
 def update_concurrent_players(sender, instance, *args, **kwargs) -> None:
     print("update_concurrent_players")
-    if not instance.status == MatchStatus.FINISHED:
+    calculate(match=instance)
+
+
+def calculate(match):
+    if match.status != MatchStatus.FINISHED:
         return
 
-    match_players = TeamPlayerAction.objects.filter(match=instance).select_related('player')
+    result: MatchResult = match.matchresult
+    cp = CorrectPredictScore.load()
+    print('----------------------' * 5)
+    print(result)
 
-    team_1 = match_players.filter(player__team=instance.team_1)
-    # team_2 = match_players.filter(player__team=instance.team_2)
-
-    predicts = PredictionArrange.objects.filter(match=instance)
+    predicts = PredictionArrange.objects.filter(match=match, is_processed=False)
+    print(predicts)
     for predict in predicts:
-        point_1 = calculate_points(predict=predict.predict_team_1, match_players=team_1, )
-        # point_2 = calculate_points(predict=predict.predict_team_2, match_players=team_2)
-        points = point_1  # + point_2
+        print('----------------------')
+        points = 0
 
-        # post_save.disconnect(update_concurrent_players, sender=Match)
-        # predict.point = points
-        # predict.save()
-        #
-        # predict.player.score += points
-        # predict.player.save()
-        # post_save.connect(update_concurrent_players, sender=Match)
+        bpi = result.best_player_id == predict.best_player.id
+        points += cp.best_player_score if bpi else cp.best_player_negative_score
+
+        penalty = result.is_penalty == predict.is_penalty
+        points += cp.penalty_score if penalty else cp.penalty_negative_score
+
+        winner = result.winner == predict.winner
+        points += cp.best_player_score if winner else cp.correct_winner_negative_score
+
+        gt1 = result.team_1_goals == predict.team_1_goals
+        gt2 = result.team_2_goals == predict.team_2_goals
+        points += cp.final_result_score if gt1 and gt2 else cp.final_result_negative_score
+
+        ar1 = result.arrange_1_list == predict.arrange_1_list
+        points += cp.arrange_score if ar1 else cp.arrange_negative_score
+        ar2 = result.arrange_2_list == predict.arrange_2_list
+        points += cp.arrange_score if ar2 else cp.arrange_negative_score
+
+        ga1 = result.goal_assist_1_list == predict.goal_assist_1_list
+        points += cp.assist_goal_score if ga1 else cp.assist_goal_negative_score
+        ga2 = result.goal_assist_2_list == predict.goal_assist_2_list
+        points += cp.assist_goal_score if ga2 else cp.assist_goal_negative_score
+
+        yc1 = result.yellow_card_1_list == predict.yellow_card_1_list
+        points += cp.yellow_card_score if yc1 else cp.yellow_card_negative_score
+        yc2 = result.yellow_card_2_list == predict.yellow_card_2_list
+        points += cp.yellow_card_score if yc2 else cp.yellow_card_negative_score
+
+        gs1 = result.goal_1_list == predict.goal_1_list
+        points += cp.goal_score if gs1 else cp.goal_negative_score
+        gs2 = result.goal_2_list == predict.goal_2_list
+        points += cp.goal_score if gs2 else cp.goal_negative_score
+
+        rc1 = result.red_card_1_list == predict.red_card_1_list
+        points += cp.red_card_score if rc1 else cp.red_card_negative_score
+        rc2 = result.red_card_2_list == predict.red_card_2_list
+        points += cp.red_card_score if rc2 else cp.red_card_negative_score
+
+        ch1 = result.change_1_list == predict.change_1_list
+        points += cp.change_player_score if ch1 else cp.change_player_negative_score
+        ch2 = result.change_2_list == predict.change_2_list
+        points += cp.change_player_score if ch2 else cp.change_player_negative_score
+
+        predict.point = points
+        predict.is_processed = True
+        predict.save()
+
+        predict.player.score += points
+        predict.player.save()
+        print(predict.player.score, predict.point)
+        print(f'\x1b[0;37;40m {predict.player.username} points:', points)
+
+    result.is_processed = True
+    result.save()
