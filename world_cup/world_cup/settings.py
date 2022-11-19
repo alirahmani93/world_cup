9ba61c57b2bc3645a1d80fc388cc78a0dc690807
root <ali93rahmani@gmail.com>
2022-11-20 01:48:40 +0330

    fix CORS

--- world_cup/world_cup/settings.py
+++ world_cup/world_cup/settings.py
@@ -190,8 +190,9 @@ SEND_SMS_URL = 'https://api.kavenegar.com/v1/{}/sms/send.json'.format(SMS_PANEL_
 # CORS
 #CORS_ORIGIN_ALLOW_ALL = bool(os.getenv("CORS_ORIGIN_ALLOW_ALL", 0))
 CORS_ALLOW_ALL_ORIGINS= True
-#CORS_ORIGIN_WHITELIST = os.getenv("CORS_ORIGIN_WHITELIST", "http://127.0.0.1:8001").split(",")
+CORS_ORIGIN_WHITELIST = os.getenv("CORS_ORIGIN_WHITELIST", "http://127.0.0.1:8001").split(",")
 #CORS_ALLOWED_ORIGINS=['*']
 
 CORS_ORIGIN_ALLOW_ALL=True
 #CORS_ORIGIN_WHITELIST=
+
